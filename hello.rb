fc576ff2d7e052b24e00382cb96bbd68460b3f96
kkurakata <k-kurakata@hykwlab.org>
2014-09-25 09:33:28 +0000

    Add Rakuten Sample

--- hello.rb
+++ hello.rb
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
 require 'sinatra'
+require 'rakuten_web_service'
 
-get '/' do 
-   "Hello World!"
+#参考: https://github.com/k2works/sinatra_rakuten_api
+
+get '/' do
+  RakutenWebService.configuration do |c|
+    c.application_id = ENV["APPID"]
+    c.affiliate_id = ENV["AFID"]
+  end
+
+  # Use genre id to fetch genre object
+  @rankings = RakutenWebService::Ichiba::Item.ranking(:age => 30, :sex => 0)
+  erb :item_ranking
 end
