9e988be8b44f702d2250d0edb3b83b5448230d72
ucchi <sabu3260@gmail.com>
2014-09-26 06:39:53 +0000

    ui changed.

--- hello.rb
+++ hello.rb
@@ -4,13 +4,22 @@ require 'rakuten_web_service'
 
 #参考: https://github.com/k2works/sinatra_rakuten_api
 
-get '/' do
-  RakutenWebService.configuration do |c|
-    c.application_id = ENV["APPID"]
-    c.affiliate_id = ENV["AFID"]
-  end
+get '/result' do
+  puts params[:age]
+  puts params[:sex]
 
-  # Use genre id to fetch genre object
-  @rankings = RakutenWebService::Ichiba::Item.ranking(:age => 30, :sex => 0)
+  RakutenWebService.configuration do |c|
+        c.application_id = ENV["APPID"]
+        c.affiliate_id = ENV["AFID"]
+  end
+  # Use genre id to fetch genre object                                                                                                                          
+  @rankings = RakutenWebService::Ichiba::Item.ranking(:age => params[:age], :sex => params[:sex])
   erb :item_ranking
 end
+
+get '/' do                                                                                  
+  erb :index
+end
+
+
+
